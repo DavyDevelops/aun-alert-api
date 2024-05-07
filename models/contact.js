67ec1521d3c6599305adfc1044c048b6cbd80f45
david nyong <davydevelops@gmail.com>
2024-05-07 18:19:38 +0100

    required commented out

--- models/contact.js
+++ models/contact.js
@@ -3,16 +3,16 @@ import mongoose from 'mongoose';
 const ContactSchema = new mongoose.Schema({
     name: {
         type: String,
-        require: true
+        // require: true
     },
     email: {
         type: String,
-        require: true,
+        // require: true,
         unique: true
     },
     phone: {
         type: String,
-        require: true,
+        // require: true,
         unique: true
     },
     address: {
